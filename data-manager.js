db6316b3a01672e90708e21fa08f0051115ce373
AltafHussainAI <altafrahi857@gmail.com>
2026-05-04 14:20:54 +0500

    Update data-manager.js

--- data-manager.js
+++ data-manager.js
@@ -2,7 +2,7 @@
 // DATA MANAGER - WITH GITHUB GIST AUTO-BACKUP
 // =============================================
 
-const GITHUB_TOKEN = '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';
+const GITHUB_TOKEN = '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';
 const GIST_ID      = '28384e87e0185be5e8e8a663c106c11b';
 const GIST_FILE    = 'movie-data.json';
 const STORAGE_KEY  = 'movieTrackerData';
